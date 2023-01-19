feab1ea73484e675f90ca68722c62b1e432548fa
17982529+alexm622@users.noreply.github.comAlex Comeauy <17982529+alexm622@users.noreply.github.com>
2023-01-18 19:30:57 -0500

    rust :)

---
+++ sdks/rust.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o /tmp/rustup.sh
+
+sh /tmp/rustup.sh -y
--- setup.zsh
+++ setup.zsh
@@ -1,19 +1,41 @@
 #!/bin/zsh
 
+repo_location="/home/$(whoami)/setup-script"
 
-echo "starting setup install"
-
-read -q "config?Do you already have a config file (config.txt) containing your github information (y/n):"
-
-if [[ $config = y ]];
+if [[ $(pwd) != $repo_location ]];
 then
-	#read from config
-	email=$(head -n 1 config.txt)
-	name=$(head -n 2 config.txt)
-else
-	read "email?Enter your email for github: "
-	read "name?Enter your name for github: "
+	echo "starting setup install"
+
+	read -q "config?Do you already have a config file (config.txt) containing your github information (y/n):"
+
+	if [[ $config = y ]];
+	then
+		#read from config
+		email=$(head -n 1 config.txt)
+		name=$(head -n 2 config.txt)
+	else
+		#read from stdin
+		read "email?Enter your email for github: "
+		read "name?Enter your name for github: "
+	fi
+
+	#set username and password
+	git config --global user.email "$email"
+	git config --global user.name "$name"
+
+	#clone this repo
+	if [ ! -d "/home/$(whoami)/setup-script" ];
+	then
+		echo cloning repo
+		git clone https://github.com/alexm622/setup-script ~/setup-script
+	fi
+
+	cd /home/$(whoami)/setup-script
+
+	zsh ./setup.zsh
+	return
 fi
 
-git config --global user.email "$email"
-git config --global user.name "$name"
+#start installing stuff
+
+sh ./sdks/install.sh
