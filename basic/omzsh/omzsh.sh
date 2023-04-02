5ee3124d2e650767f3f6c0839bbeee06ece47187
Alex <17982529+alexm622@users.noreply.github.com>
2023-04-02 02:25:09 +0000

    omzsh updated

--- basic/omzsh/omzsh.sh
+++ basic/omzsh/omzsh.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 wget https://raw.githubusercontent.com/ChesterYue/ohmyzsh-theme-passion/master/passion.zsh-theme ~/.oh-my-zsh/themes/
+
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+#patch passion
+patch ~/.oh-my-zsh/passion.zsh-theme ./passion.diff
 
 #patch zshrc
 patch ~/.zshrc ./zshrc.diff
--- basic/omzsh/update_diff.sh
+++ basic/omzsh/update_diff.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 diff zshrc.zsh-template ~/.zshrc > zshrc.diff
+
+diff passion.zsh-theme ~/.oh-my-zsh/themes/passion.zsh-theme > passion.diff
