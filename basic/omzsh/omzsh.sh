5cfb2e31f1474c422cada2229970b47b0a97fe1d
Alex <17982529+alexm622@users.noreply.github.com>
2023-04-04 03:41:33 +0000

    merging

--- basic/omzsh/omzsh.sh
+++ basic/omzsh/omzsh.sh
@@ -4,5 +4,11 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 wget https://raw.githubusercontent.com/ChesterYue/ohmyzsh-theme-passion/master/passion.zsh-theme ~/.oh-my-zsh/themes/
 
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+#patch passion
+patch ~/.oh-my-zsh/passion.zsh-theme ./passion.diff
+
 #patch zshrc
 patch ~/.zshrc ./zshrc.diff
